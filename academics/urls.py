27b7566580e6401abac79502bdf4ee35ec62cff3
Ichoku Somtoochukwu <ichoku.somtoochukwu@hugeinc.com>
2026-01-23 11:35:43 +0100

    views added

--- academics/urls.py
+++ academics/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 
 urlpatterns = [
-    path('courses/', views.courses_with_enrollment_status),
+    path('courses/', views.CourseListView.as_view()),
     path('courses/<uuid:pk>/', views.CourseDetailView.as_view()),
     path('enrollments/', views.EnrollmentListView.as_view()),
     path('enrollments/current/', views.current_enrollments),
