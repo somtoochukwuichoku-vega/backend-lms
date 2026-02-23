6430158af1690263ef3c101e1897b49234024cb6
Ichoku Somtoochukwu <ichoku.somtoochukwu@hugeinc.com>
2026-02-23 09:27:56 +0100

    add installment payment

--- payments/urls.py
+++ payments/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from academics.views import stripe_webhook
-from .views import CreateCheckoutSessionView, ProcessInstallmentView
+from .views import CreateCheckoutSessionView, CreateInstallmentSessionView, ProcessInstallmentView
 
 urlpatterns = [
     path('create-checkout-session/<uuid:course_id>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session'),
+    path('create-installment-session/<uuid:course_id>/', CreateInstallmentSessionView.as_view(), name='create-installment-session'),
     path('process-installment/<uuid:transaction_id>/', ProcessInstallmentView.as_view(), name='process-installment'),
     path('webhook/', stripe_webhook, name='stripe-webhook'),
 ]
